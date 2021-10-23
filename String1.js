5c61005073e39310050feada128f85ca5ca17be3
prasetiyojs <prassonly@gmail.com>
2021-10-23 15:58:57 +0700

    add string1 and update file

---
+++ String1.js
@@ -0,0 +1,11 @@
+// Add your code here
+
+let quoteStart = 'Don\'t judge each day by the harvest you reapp. ';
+let quoteEnd = 'Just be grateful and give the best of you do.';
+let finalQuote = quoteStart + quoteEnd;
+
+// Don't edit the code below here!
+
+const section = document.querySelector('section');
+let para1 = document.createElement('p');
+para1.textContent = finalQuote;
