f32962e2214cc73305f2cae727d3aa98ccd47832
prasetiyojs <prassonly@gmail.com>
2021-10-23 16:14:41 +0700

    update file

--- String1.js
+++ String1.js
@@ -9,3 +9,4 @@ let finalQuote = quoteStart + quoteEnd;
 const section = document.querySelector('section');
 let para1 = document.createElement('p');
 para1.textContent = finalQuote;
+section.appendChild(para1);
