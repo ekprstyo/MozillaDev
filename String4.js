534f42a4bf126274455bc38133a2183dc79843ca
prasetiyojs <prassonly@gmail.com>
2021-10-28 16:23:36 +0700

    Update

--- String4.js
+++ String4.js
@@ -1,15 +1,15 @@
 let theorem = 'Pythagorean theorem';
 
-        let a = 5;
-        let b = 8;
+let a = 5;
+let b = 8;
 
-        let myString = `Using ${ theorem }, we can work out that that if the two shortest sides of a right-angled triangle have lengths of ${ a } and ${ b }, the length of the hypotenuse is ${ Math.sqrt(a**2 + b**2) }.`;
+let myString = `Using ${ theorem }, we can work out that that if the two shortest sides of a right-angled triangle have lengths of ${ a } and ${ b }, the length of the hypotenuse is ${ Math.sqrt(a**2 + b**2) }.`;
 
-        // Don't edit the codes below here!
+// Don't edit the codes below here!
 
-        const section = document.querySelector('section');
+const section = document.querySelector('section');
 
-        let para1 = document.createElement('p');
-        para1.textContent = myString;
+let para1 = document.createElement('p');
+para1.textContent = myString;
 
-        section.appendChild(para1);
+section.appendChild(para1);
